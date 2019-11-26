e92adfd70bb94e3e9641114b55796452882c2e12
Michael <miservilla@gmail.com>
2019-11-26 06:16:26 -0700

    Initial commit

---
+++ src/Trimmer.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ * Java program to trim leading line numbers from file.
+ */
+
+public class Trimmer
+{
+    public static void main(String[] args)
+    {
+        String fileName;
+        int trimNumber;
+        Scanner console = new Scanner(System.in);
+        System.out.print("Type in name of file: ");
+        fileName = console.nextLine();
+        System.out.print("Type in digit size to trim: ");
+        trimNumber = console.nextInt();
+        File file = new File(fileName);
+
+        try
+        {
+            Scanner scnr = new Scanner(file);
+
+            while (scnr.hasNextLine())
+            {
+                String line = scnr.nextLine();
+                System.out.println(line.substring(trimNumber));
+                try (FileWriter writer = new FileWriter("Trimmed" + fileName,true);
+                     BufferedWriter bw = new BufferedWriter(writer))
+                {
+                    {
+                        bw.write(line.substring(trimNumber) + "\n");
+                    }
+                }
+            }
+            scnr.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        File oldfile = new  File("Trimmed" + fileName);
+        oldfile.renameTo(file);
+
+    }
+}

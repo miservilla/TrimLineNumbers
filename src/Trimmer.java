b8f1afac096e6be2439f21cf9be641c4d0b392a2
Michael Servilla <miservilla@gmail.com>
2020-01-09 11:47:02 -0700

    Deleted commented lines out.

--- src/Trimmer.java
+++ src/Trimmer.java
@@ -10,7 +10,6 @@ public class Trimmer
     public static void main(String[] args)
     {
         String fileName;
-        int trimNumber;
         Scanner console = new Scanner(System.in);
         System.out.print("Type in name of file: ");
         fileName = console.nextLine();
